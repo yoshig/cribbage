ff5dc24b26e27c15be4841f939f4b5692d48c32a
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-02-02 12:13:49 -0800

    Added runs point scoring

--- crib_counter.rb
+++ crib_counter.rb
@@ -24,62 +24,74 @@ class CribCounter
 
   def card_vals_for_runs
     non_number_cards = {
-      "A" => 1
-      "J" => 11
-      "Q" => 12
-      "K" => 13
+      "A" => 1,
+      "J" => 11,
+      "Q" => 12,
+      "K" => 13,
     }
     @hand.map do |card|
-      non_number_cards.include?[card[0]]
+      non_number_cards.include?(card) ? non_number_cards[card] : card.to_i
     end
   end
 
   def fifteens
-  	(2..5).each do |num_cards|
-  		all_combos = card_vals_for_fifteens.combination(num_cards).to_a
-      all_combos.each do |combo|
-        @total_points += 2 if combo.inject(:+) == 15
+    0.tap do |fifteen_points|
+    	(2..5).each do |num_cards|
+    		all_combos = card_vals_for_fifteens.combination(num_cards).to_a
+        all_combos.each do |combo|
+          fifteen_points += 2 if combo.inject(:+) == 15
+        end
       end
     end
-
-    @total_points
   end
 
   def pairs
-    all_combos = @hand.combination(2).to_a
-    all_combos.each do |combo|
-      @total_points += 2 if combo[0] == combo[1]
+    0.tap do |pairs|
+      all_combos = @hand.combination(2).to_a
+      all_combos.each do |combo|
+        pairs += 2 if combo[0] == combo[1]
+      end
     end
+  end
 
-    @total_points
+  def runs_from_sets(run_vals, n)
+    combos = run_vals.combination(n).to_a
+    combos.select do |set|
+      set.last - set.first == n - 1
+    end.count
   end
 
   def runs
-    #change values to 1-13
-
-    (3..5).each do |run_length|
-      all_combos = @hand.combination(run_length).to_a
-      all_combos.map! { |a| a.sort }
-      p all_combos
+    run_cards = card_vals_for_runs.sort
+    (3..5).to_a.reverse.each do |combo_length|
+      total_runs = runs_from_sets(run_cards, combo_length)
+      if total_runs > 0
+        return combo_length * total_runs
+      end
     end
+
+    0
+  end
+
+  def flush
+
   end
 
   def all_points
-    fifteens
-    pairs
+    fifteens + pairs + runs
   end
 
 end
 
 crib_hand = CribCounter.new([
-  ["3", "H"],
-  ["2", "H"],
-  ["K", "S"],
-  ["K", "D"],
+  ["10", "H"],
+  ["J", "H"],
+  ["J", "S"],
+  ["Q", "D"],
   ["K", "C"]
   ])
 
-p crib_hand.card_vals_for_fifteens
+# p crib_hand.card_vals_for_fifteens
 # p crib_hand.fifteens
 # p crib_hand.all_points
 p crib_hand.runs
