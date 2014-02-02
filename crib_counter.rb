8461ce67311780b0d3ea6ec31eb3ed7a685521a1
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2014-02-02 11:38:26 -0800

    moved to cribbage folder

--- crib_counter.rb
+++ crib_counter.rb
@@ -2,32 +2,41 @@ class CribCounter
   attr_accessor :hand
 
   def initialize(hand)
-    @hand = hand
+    @hand = hand.map { |card| card[0] }
+    @suits = hand.map { |card| card[1] }
     @total_points = 0
   end
 
-  def card_nums
+  def card_vals_for_fifteens
   	only_num_vals = []
   	@hand.each do |card|
-  		if card[0] == "A"
+  		if card == "A"
         only_num_vals << 1
-      elsif card[0].to_i == 0
+      elsif card.to_i == 0
         only_num_vals << 10
       else
-        only_num_vals << card[0].to_i
+        only_num_vals << card.to_i
       end
   	end
 
     only_num_vals
   end
 
-  def card_values
-    @hand.map { |card| card[0] }
+  def card_vals_for_runs
+    non_number_cards = {
+      "A" => 1
+      "J" => 11
+      "Q" => 12
+      "K" => 13
+    }
+    @hand.map do |card|
+      non_number_cards.include?[card[0]]
+    end
   end
 
   def fifteens
   	(2..5).each do |num_cards|
-  		all_combos = card_nums.combination(num_cards).to_a
+  		all_combos = card_vals_for_fifteens.combination(num_cards).to_a
       all_combos.each do |combo|
         @total_points += 2 if combo.inject(:+) == 15
       end
@@ -37,7 +46,7 @@ class CribCounter
   end
 
   def pairs
-    all_combos = card_values.combination(2).to_a
+    all_combos = @hand.combination(2).to_a
     all_combos.each do |combo|
       @total_points += 2 if combo[0] == combo[1]
     end
@@ -49,7 +58,7 @@ class CribCounter
     #change values to 1-13
 
     (3..5).each do |run_length|
-      all_combos = card_values.combination(run_length).to_a
+      all_combos = @hand.combination(run_length).to_a
       all_combos.map! { |a| a.sort }
       p all_combos
     end
@@ -70,7 +79,7 @@ crib_hand = CribCounter.new([
   ["K", "C"]
   ])
 
-p crib_hand.card_nums
+p crib_hand.card_vals_for_fifteens
 # p crib_hand.fifteens
 # p crib_hand.all_points
 p crib_hand.runs
